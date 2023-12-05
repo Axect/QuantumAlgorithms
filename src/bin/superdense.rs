286db610f4e139c1aefb1b447212fc69935d00e5
axect <ax2ct@outlook.com>
2023-12-05 13:53:29 +0900

    Refactor superdense

--- src/bin/superdense.rs
+++ src/bin/superdense.rs
@@ -30,16 +30,16 @@ fn main() -> Result<(), CircuitError> {
     let a = l.h(a);
 
     // Measure a and b
-    let (_, m_handle_a) = l.measure(a);
-    let (_, m_handle_b) = l.measure(b);
+    let (_, handle_a) = l.measure(a);
+    let (_, handle_b) = l.measure(b);
 
     // Calculate final state
     let alice_states = [(0, 0), (0, 1), (1, 0), (1, 1)];
 
     for (alice1, alice2) in alice_states.into_iter() {
         let (_, measured) = l.calculate_state_with_init([(&e1, alice1), (&e2, alice2)]);
-        let (result_a, p_a) = measured.get_measurement(m_handle_a);
-        let (result_b, p_b) = measured.get_measurement(m_handle_b);
+        let (result_a, p_a) = measured.get_measurement(handle_a);
+        let (result_b, p_b) = measured.get_measurement(handle_b);
         println!(
             "Input: {}{}, Measured: {}{} with probability: {:.4}",
             alice1,
