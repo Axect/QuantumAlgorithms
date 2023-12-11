22fa2036adee73f9a66398462a0e58f432bae2bc
axect <ax2ct@outlook.com>
2023-12-11 21:05:22 +0900

    Deutsch-Jozsa via RustQIP

--- src/bin/deutsch_jozsa.rs
+++ src/bin/deutsch_jozsa.rs
@@ -1,43 +1,107 @@
-use qip::{prelude::*, rand::distributions::Bernoulli};
+use qip::prelude::*;
 use peroxide::fuga::*;
 use std::result::Result;
+use dialoguer::{theme::ColorfulTheme, Input, Select};
 
-fn main() {
+const N: usize = 10;
 
-}
-
-fn gen_f_constant(n: usize) -> Box<dyn Fn(&[usize]) -> usize> {
-    Box::new(move |x: &[usize]| -> usize {
-        0
-    })
-}
-
-fn gen_f_balanced(n: usize) -> Box<dyn Fn(&[usize]) -> usize> {
-    let bernoulli = Bernoulli(0.5);
-    let y = bernoulli.sample(2usize.pow((n-1) as u32)).iter().map(|&x| x as usize).collect::<Vec<usize>>();
-    let y_not = y.iter().map(|&x| 1 - x).collect::<Vec<usize>>();
-    let y = y.into_iter().chain(y_not.into_iter()).collect::<Vec<usize>>();
-
-    Box::new(move |x: &[usize]| -> usize {
-        let mut number = 0usize;
-        for i in 0..n {
-            number += x[i] * 2usize.pow((n-i-1) as u32);
+macro_rules! apply_if {
+    ($cond:expr, $action:expr, $val:expr) => {
+        if $cond {
+            $action
+        } else {
+            $val
         }
-        y[number]
-    })
+    };
 }
 
-fn real_mat_to_complex_mat<const N: usize>(mat: [f64; N]) -> [Complex<f64>; N] {
-    let mut complex_mat = [Complex::new(0.0, 0.0); N];
-    for (i, r) in mat.iter().enumerate() {
-        complex_mat[i] = Complex::new(*r, 0.0);
+macro_rules! apply_oracle {
+    ($l:ident;$n:ident;$case:expr;$($q:ident=>$i:expr),+; $a:ident) => {{
+        $(let $q = $l.h($q);)+
+        let $a = $l.h($a);
+
+        // Oracle
+        let (q, a) = match $case {
+            Case::Balanced => {
+                let b = Uniform(1i32, 2i32.pow($n as u32)-1).sample(1)[0] as usize;
+                let b_str = format!("{:0width$b}", b, width = $n);
+                let b_chars = b_str.chars().collect::<Vec<char>>();
+                $(let $q = apply_if!(b_chars[$i] == '1', $l.x($q), $q);)+
+                $(let ($q, $a) = $l.cnot($q, $a)?;)+
+                $(let $q = apply_if!(b_chars[$i] == '1', $l.x($q), $q);)+
+
+                let q = $l.merge_registers([$($q),+]).unwrap();
+                let a = $a;
+                (q, a)
+            }
+            Case::Constant => {
+                let b = Uniform(0i32, 1i32).sample(1)[0] as usize;
+                let $a = apply_if!(b == 1, $l.x($a), $a);
+                let q = $l.merge_registers([$($q),+]).unwrap();
+                let a = $a;
+                (q, a)
+            }
+        };
+        (q, a)
+    }};
+}
+
+fn main() -> Result<(), CircuitError> {
+    let case = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Choose case")
+        .default(0)
+        .items(&[Case::Balanced, Case::Constant])
+        .interact()
+        .unwrap();
+    let case = match case {
+        0 => Case::Balanced,
+        _ => Case::Constant
+    };
+    
+    let mut l = LocalBuilder::<f64>::default();
+    let q1 = l.qubit();
+    let q2 = l.qubit();
+    let q3 = l.qubit();
+    let q4 = l.qubit();
+    let q5 = l.qubit();
+    let q6 = l.qubit();
+    let q7 = l.qubit();
+    let q8 = l.qubit();
+    let q9 = l.qubit();
+    let q10 = l.qubit();
+    let a = l.qubit();
+    let a = l.x(a);
+
+    let (q, a) = apply_oracle!(l; N; case; q1=>0, q2=>1, q3=>2, q4=>3, q5=>4, q6=>5, q7=>6, q8=>7, q9=>8, q10=>9; a);
+
+    let q = l.h(q);
+
+    let (_, handle) = l.measure(q);
+
+    // Calculate final state
+    let (_, measured) = l.calculate_state();
+    let (result, p) = measured.get_measurement(handle);
+    println!(
+        "Measured: {:0width$b} with probability {:.4}",
+        result,
+        p,
+        width = N
+    );
+
+    Ok(())
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum Case {
+    Balanced,
+    Constant
+}
+
+impl std::fmt::Display for Case {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Case::Balanced => write!(f, "Balanced"),
+            Case::Constant => write!(f, "Constant"),
+        }
     }
-    complex_mat
-}
-
-#[allow(non_snake_case)]
-fn U<F: Fn(usize) -> usize, const N: usize>(f: F, n: usize) -> [Complex<f64>; N] {
-    let mut m = eye(2usize.pow(n as u32 + 1));
-
-    todo!()
 }
